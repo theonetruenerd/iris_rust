bd5459edcc4207812e5dc200f5d8ecf935159e1d
theonetruenerd <tarunchapman@hotmail.com>
2025-11-29 13:54:40 +0000

    Initial commit; led flash working

---
+++ src/bin/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+use esp_println::println;
+use esp_hal::gpio::{Level, Output};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    // generator version: 1.0.1
+    esp_println::logger::init_logger_from_env();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let mut led = Output::new(peripherals.GPIO2, Level::Low, Default::default());
+    let mut led_2 = Output::new(peripherals.GPIO1, Level::Low, Default::default());
+
+    loop {
+        // led.toggle();
+        println!("Toggling LED");
+        led_2.toggle();
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
+}
