9a4b4b88af544775c9ac3bcb4c7981552d57551d
theonetruenerd <tarunchapman@hotmail.com>
2025-11-29 18:48:33 +0000

    Displays circle and hello world on screen

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -58,28 +58,27 @@ use esp_hal::{main, spi};
 use esp_hal::delay::Delay;
 use esp_hal::time::{Duration, Instant, Rate};
 use esp_println::println;
-use esp_hal::gpio::{Level, Output};
+use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::spi::master::Spi;
+
+use esp_hal::spi::master::Config as SpiConfig;
+use esp_hal::spi::Mode as SpiMode;
+use mipidsi::interface::SpiInterface;
+use mipidsi::{Builder, models::ST7789};
+use embedded_graphics::{
+    prelude::*,
+    pixelcolor::Rgb565,
+    mono_font::{ascii::FONT_10X20, MonoTextStyle},
+    text::Text,
+    primitives::{Circle, PrimitiveStyle, Primitive},
+};
+use mipidsi::options::{ColorInversion, Orientation, Rotation};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-pub struct DummyTimesource();
-
-impl TimeSource for DummyTimesource {
-    fn get_timestamp(&self) -> Timestamp {
-        Timestamp {
-            year_since_1970: 0,
-            zero_indexed_month: 0,
-            zero_indexed_day: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0,
-        }
-    }
-}
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -90,48 +89,54 @@ fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    let cs = Output::new(peripherals.GPIO12, Level::High, Default::default());
-    let sck = peripherals.GPIO40;
-    let mosi = peripherals.GPIO14;
-    let miso = peripherals.GPIO39;
-
-    let spi_bus = Spi::new(
+    let spi = Spi::new(
         peripherals.SPI2,
-        spi::master::Config::default()
-            .with_frequency(Rate::from_khz(400))
-            .with_mode(spi::Mode::_0),
+        SpiConfig::default()
+            .with_frequency(Rate::from_mhz(10))
+            .with_mode(SpiMode::_0),
         )
         .unwrap()
-        .with_sck(sck)
-        .with_mosi(mosi)
-        .with_miso(miso);
+        .with_sck(peripherals.GPIO36)
+        .with_mosi(peripherals.GPIO35);
 
-    let spi_dev = ExclusiveDevice::new(spi_bus, cs, Delay::new()).unwrap();
-    let sdcard = SdCard::new(spi_dev, Delay::new());
+    let cs = Output::new(peripherals.GPIO37, Level::High, OutputConfig::default());
+    let dc = Output::new(peripherals.GPIO34, Level::Low, OutputConfig::default());
+    let rst = Output::new(peripherals.GPIO33, Level::Low, OutputConfig::default());
 
-    println!("Init SD card controller and retrieve card info...");
-    let sd_size = sdcard.num_bytes().unwrap();
-    println!("SD card size: {} bytes", sd_size);
+    let mut delay = Delay::new();
 
-    let volume_mgr = VolumeManager::new(sdcard, DummyTimesource());
+    let bl = Output::new(peripherals.GPIO38, Level::High, OutputConfig::default());
 
-    let volume0 = volume_mgr.open_volume(VolumeIdx(0)).unwrap();
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
 
-    let root_dir = volume0.open_root_dir().unwrap();
+    let mut buffer = [0u8; 512];
 
-    let mut my_file = root_dir.open_file_in_dir(
-        "Test.txt",
-        Mode::ReadWriteCreateOrTruncate
-        )
+    let di = SpiInterface::new(spi_device, dc, &mut buffer);
+
+    let mut display = Builder::new(ST7789, di)
+        .reset_pin(rst)
+        .invert_colors(ColorInversion::Inverted)
+        .orientation(Orientation::new().rotate(Rotation::Deg90))
+        .init(&mut delay)
         .unwrap();
 
-    let line = "Hello World!";
-    if let Ok(()) = my_file.write(line.as_bytes()) {
-        my_file.flush().unwrap();
-        println!("Wrote: {}", line);
-    }
+    display.clear(Rgb565::WHITE).unwrap();
+
+    let text_style = MonoTextStyle::new(&FONT_10X20, Rgb565::BLACK);
+
+    Text::new("Hello World!", Point::new(10, 50), text_style)
+        .draw(&mut display)
+        .unwrap();
+
+    let circle_style = PrimitiveStyle::with_stroke(Rgb565::RED, 5);
+
+    Circle::new(Point::new(50, 80), 60)
+        .into_styled(circle_style)
+        .draw(&mut display)
+        .unwrap();
 
     loop {
+        println!("Hello World!");
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
